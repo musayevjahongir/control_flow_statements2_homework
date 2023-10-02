e8729d2838ddce6d253f5d87d17bc62d17e439e8
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-02 19:54:16 +0500

    kattasini topish

--- if01.py
+++ if01.py
@@ -8,4 +8,11 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    d=a
+    if b>d:
+        d=b
+    if c>d:
+        d=c
+    return d
+print(main(1,4,2))
+print(main(-5,-4,-2))
