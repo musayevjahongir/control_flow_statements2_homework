df419a7c8b1e533c145673660142fd359f425555
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-02 20:12:25 +0500

    ortachasini topish

--- if03.py
+++ if03.py
@@ -8,4 +8,11 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    d=a
+    if a>b and b>c:
+        d=b
+    if a>c and c>b:
+        d=c
+    return d
+print(main(3,7,1))
+print(main(5,5,-1))
