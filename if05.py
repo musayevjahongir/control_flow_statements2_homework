b0c585522007c70a10b3141c9ce19841f8b757ab
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-02 20:26:15 +0500

    katta raqamni aniqlash

--- if05.py
+++ if05.py
@@ -6,4 +6,20 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1=n%10
+    x2=n//10%10
+    x3=n//100%10
+    x4=n//1000%10
+    x5=n//10000
+    a=x1
+    if x2>a:
+        a=x2
+    if x3>a:
+        a=x3
+    if x4>a:
+        a=x4
+    if x5>a:
+        a=x5
+    return a
+print(main(23546))
+print(main(17921))
